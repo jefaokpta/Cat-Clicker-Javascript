3ab72989b7d846f8a7a37a8231593f2472ae48eb
Jefferson Alves <jefaokpta@hotmail.com>
2018-05-21 15:47:00 -0300

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,128 @@
+window.onload=function(){
+    var model={
+        cat,
+        cats:JSON.parse(localStorage.getItem('cats')),
+        getAll:function(){
+            return this.cats;
+        },
+        update:function(){
+            localStorage.setItem('cats',JSON.stringify(this.cats));
+        }
+    }
+    var oct={
+        getCats:function(){
+            return model.getAll();
+        },
+        init:function(){
+            view.init();
+            view.buildList();
+            this.currentCat(this.getCats()[0]);
+            view.renderDiv();
+        },
+        soma:function(cat){
+            cat.num++;
+            model.update();
+            view.render(cat);
+        },
+        currentCat:function(cat){
+            model.cat=cat;
+        },
+        getCurrentCat:function(){
+            return model.cat;
+        },
+        saveCat:function(){
+            model.update();
+            view.renderDiv();
+            view.buildList();
+        }
+    }
+    var view={
+        ul:document.createElement('ul'),
+        init:function(){
+            
+            var btn=document.querySelector('#btnadmin');
+            var divadmin=document.querySelector('#divadmin');
+            var name=document.querySelector('#name');
+            var img=document.querySelector('#img');
+            var count=document.querySelector('#count');
+            var btncancel=document.querySelector('#cancel');
+            var btnsave=document.querySelector('#save');
+
+            btn.addEventListener('click',function(){
+                divadmin.style.display='block';
+                var cc=oct.getCurrentCat();
+                name.value=cc.cat;
+                img.value=cc.photo;
+                count.value=cc.num;
+            });
+            btncancel.addEventListener('click',function(){
+                divadmin.style.display='none';
+            });
+            var ul=this.ul;
+            btnsave.addEventListener('click',function(){
+                while (ul.firstChild) {
+                    ul.removeChild(ul.firstChild);
+                }
+                var cat=oct.getCurrentCat();
+                cat.cat=name.value;
+                cat.num=count.value;
+                oct.saveCat();
+                divadmin.style.display='none';
+            });            
+        },
+        buildList:function(){          
+            oct.getCats().forEach(cat => {               
+                var li=document.createElement('li');
+                var a=document.createElement('a');
+                a.setAttribute('href','#');
+                a.addEventListener('click',function(){
+                    document.querySelector('#cat').innerHTML=null;
+                    var img=document.createElement('img');
+                    img.addEventListener('click',function(){oct.soma(cat)});
+                    img.setAttribute('src','img/'+cat.photo );
+                    var h2=document.createElement('h2');
+                    var txt=document.createTextNode(cat.cat);
+                    h2.appendChild(txt);
+                    var h1=document.createElement('h1');
+                    h1.setAttribute('id','cat'+cat.cat);
+                    var txt2=document.createTextNode(cat.num);
+                    h1.appendChild(txt2);
+                    oct.currentCat(cat);
+                    document.querySelector('#cat').appendChild(img);
+                    document.querySelector('#cat').appendChild(h2);
+                    document.querySelector('#cat').appendChild(h1);
+                });
+                var t=document.createTextNode(cat.cat);
+                a.appendChild(t);
+                li.appendChild(a);
+                this.ul.appendChild(li);                    
+            });
+            document.querySelector('#cats').appendChild(this.ul);
+        },
+        render:function(cat){
+            document.querySelector('#cat'+cat.cat).innerHTML=cat.num;
+        },
+        renderDiv:function(){
+            document.querySelector('#cat').innerHTML=null;
+            var cc=oct.getCurrentCat();
+            var img=document.createElement('img');
+            img.addEventListener('click',function(){oct.soma(cc)});
+            img.setAttribute('src','img/'+cc.photo );
+            var h2=document.createElement('h2');
+            var txt=document.createTextNode(cc.cat);
+            h2.appendChild(txt);
+            var h1=document.createElement('h1');
+            h1.setAttribute('id','cat'+cc.cat);
+            var txt2=document.createTextNode(cc.num);
+            h1.appendChild(txt2);
+
+            document.querySelector('#cat').appendChild(img);
+            document.querySelector('#cat').appendChild(h2);
+            document.querySelector('#cat').appendChild(h1);
+        }      
+    }
+    oct.init();
+}
+
+
+
