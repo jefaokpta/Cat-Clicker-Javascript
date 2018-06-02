5f4e849a525f07a7bf27d795520890cc53d4b86c
Jefferson Alves <jefaokpta@hotmail.com>
2018-06-02 18:32:47 -0300

    local storage manipulated

--- js/js.js
+++ js/js.js
@@ -1,4 +1,15 @@
 window.onload=function(){
+    if (localStorage.getItem('cats') == null) {
+        this.cats = [
+          { 'cat' : 'Lion' , 'photo' : 'lion.jpg' , 'num' : 0},
+          {'cat': 'Tygra', 'photo': 'tygra.jpg', 'num': 0},
+          {'cat': 'Panthro', 'photo': 'panthro.jpg', 'num': 0},
+          {'cat': 'Cheetah', 'photo': 'cheetah.jpg', 'num': 0},
+          {'cat': 'WKit', 'photo': 'wkit.jpg', 'num': 0},
+          {'cat': 'WKat', 'photo': 'wkat.jpg', 'num': 0}
+        ];
+        localStorage.setItem('cats', JSON.stringify(this.cats));
+    }
     var model={
         cat,
         cats:JSON.parse(localStorage.getItem('cats')),
